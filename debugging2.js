1bbf4042c7996ff7acd9c641f629a3528cd18550
akullu winnifred <akulluwinnifred1@gmail.com>
2024-03-19 10:07:04 +0300

    merging branches

--- debugging2.js
+++ debugging2.js
@@ -3,3 +3,8 @@ const numbers = [1, 2, 3, 4, 5 ]
     const sum = numbers.reduce((total, num) => total + num);  
     
     console.log(`The sum of numbers is: ${sum}`);
+
+
+
+
+    
---
+++ debugging2.py
@@ -0,0 +1,23 @@
+numbers = [1, 2, 3, 4, 5]
+total = 0
+
+for num in numbers:
+    total += num
+print(f"The sum of numbers is: {total}")
+
+
+
+
+def calculate_sum(numbers):
+    sum = 0
+
+    for num in numbers:
+        sum += num  
+
+    return sum  
+
+numbers = [1, 2, 3, 4, 5] 
+
+result = calculate_sum(numbers)
+
+print(f"The sum of numbers is: {result}")
