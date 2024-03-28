052f5361fba76ddca755323aef1b1bb2b757b5da
akullu winnifred <akulluwinnifred1@gmail.com>
2024-03-28 17:42:54 +0300

    update

---
+++ django.py
@@ -0,0 +1,11 @@
+#django is a python web framework used to create web applications
+#django is extremely procedural(step by step)
+#flask,pyramid,py2web are other examples of python framework
+#It follows an MVC(model, view ,controller) arrangement/policy
+#MVC allows developers to separate the application into three components: Model, View and Controller. 
+#Model-The model represents the data that your app will be working with. It's responsible for accessing the database and performing CRUD operations
+#we use methods to capture data
+#views are files that will be handling requests and  returning responses
+#A model is anything related to a database(models.py)
+#views in views.py
+#controllers are for urls which determines which views to load and views determines which models to manipulate
